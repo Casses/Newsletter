877210db0b095693d037f13ae548515bd5ac5995
Casses Atereth <Casses@users.noreply.github.com>
2025-07-08 06:49:39 -0400

    Adding a couple tweaks.

--- src/Newsletter.Data.Entities/Tag.cs
+++ src/Newsletter.Data.Entities/Tag.cs
@@ -5,7 +5,6 @@ public class Tag : BaseEntity
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     
-    // Navigation properties
+    // Navigation property for the many-to-many relationship with Event
     public ICollection<Event> Events { get; set; } = new List<Event>();
-    public ICollection<SubscriberTag> SubscriberPreferences { get; set; } = new List<SubscriberTag>();
 } 
--- src/Newsletter.Interface/Handler/INotificationHandler.cs
+++ src/Newsletter.Interface/Handler/INotificationHandler.cs
@@ -1,7 +1,7 @@
 using Newsletter.Data.Entities;
 
 namespace Newsletter.Interface.Handler;
- 
+
 public interface INotificationHandler
 {
     Task<bool> CanHandleAsync(NotificationType type);
